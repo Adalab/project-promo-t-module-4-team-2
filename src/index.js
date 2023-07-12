1ea1704d523edb44c31e6c6ff75be56a694803f6
MariaRipley <roblesb.maria@gmail.com>
2023-07-12 18:39:34 +0200

    Create async functions to get API data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('Hola');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+
+//Crear servidor
+const server = express();
+
+//Configurar servidor
+server.use(cors());
+server.use(express.json({ limit: "100mb" }));
+
+async function connectDB() {
+  const connection = await mysql.createConnection({
+    host: "sql.freedb.tech",
+    database: "freedb_project molones",
+    user: "freedb_Group2",
+    password: "nnDTSJ#mTwUg?7W",
+  });
+
+  await connection.connect();
+  console.log(`Conexión establecida ${connection.threadId}`);
+  return connection;
+}
+//Aquí falta el catch para los errores!!!
+
+//Escuchar servidor
+const PORT = 4000;
+server.listen(PORT, () => {
+  console.log("Se ha conectado al puerto " + PORT);
+});
+
+//Solicitud con endpoint
+server.get("/api/allproject", async (req, res) => {
+  const select = "SELECT * FROM project";
+  const connect = await connectDB();
+  const [result] = await connect.query(select);
+  console.log(result);
+  res.json(result);
+});
--- web/src/services/Api.js
+++ web/src/services/Api.js
@@ -12,4 +12,13 @@ const callToApi = (data) => {
     });
 };
 
-export default callToApi;
+const getAllCardsApi = () => {
+  return fetch("http://localhost:4000/api/allproject")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data.results;
+    });
+};
+
+export default { callToApi, getAllCardsApi };
