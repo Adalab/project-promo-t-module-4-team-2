18ff4d302a56ac27b10374e3d8d08841ea864ca3
SaraOsacar <saraosacar@gmail.com>
2023-07-13 18:57:47 +0200

    pre pull

--- web/src/components/pages/Home.js
+++ web/src/components/pages/Home.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import '../../styles/home.scss';
 import cover from "../../images/vibes-logo.jpg";
 import user from "../../images/urkel.jpg";
+
+
 function Home() {
   const archivedProjects = [
     {
